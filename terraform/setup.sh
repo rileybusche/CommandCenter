b2b1def2db93cd1e2c0263bda60aa906ea137274
rileybusche <riley.busche@gmail.com>
2020-01-27 19:22:25 -0600

    Updating bot name

--- terraform/setup.sh
+++ terraform/setup.sh
@@ -4,13 +4,13 @@ sudo apt install python3-pip -y
 python3 -m pip install -U discord.py
 python3 -m pip install apscheduler
 # Ticker Bot
-cd /home/ubuntu/
+cd /home/ec2-user/
 git clone https://github.com/rileybusche/TickerBotProd.git
 cd TickerBotProd/TickerPriceBot/
 touch api_key.txt
 touch token.txt
 # ShitPostBot
-cd /home/ubuntu/
+cd /home/ec2-user/
 git clone https://github.com/rileybusche/ShitPostBot.git
 cd ShitPostBot
 touch token.txt
