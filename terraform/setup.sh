666c14f9953231ac813d7f244cc220d36028ebb2
rileybusche <riley.busche@gmail.com>
2020-01-27 19:41:02 -0600

    Fixed issues using setup script in linux

--- terraform/setup.sh
+++ terraform/setup.sh
@@ -1,8 +1,9 @@
 #! /bin/bash
-sudo apt-get update
-sudo apt install python3-pip -y
-python3 -m pip install -U discord.py
-python3 -m pip install apscheduler
+sudo yum update -y
+sudo yum install git -y
+sudo yum python3-pip -y
+sudo python3 -m pip install -U discord.py
+sudo python3 -m pip install apscheduler
 # Ticker Bot
 cd /home/ec2-user/
 git clone https://github.com/rileybusche/TickerBotProd.git
