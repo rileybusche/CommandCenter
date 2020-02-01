ee6a16d1c2cf62ecbe42d7de00e38599106ba91a
rileybusche <riley.busche@gmail.com>
2020-01-31 21:38:52 -0600

    Updates

--- bot/bot.py
+++ bot/bot.py
@@ -20,7 +20,7 @@ async def on_message(message):
         return
     
     if msg == "!metrics":
-        metrics = os.popen('cat iostat')
+        metrics = os.popen('cat iostat').read()
         await channel.send(f'```fix\n{metrics}```')
 
 @client.event
