b3ac87d5acca93baf0a886f99d9750a6b628785a
rileybusche <riley.busche@gmail.com>
2020-02-01 13:09:04 -0600

    Update Scripts

---
+++ bot/scripts/update.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+if [ $1 == "spb" ]
+then
+    echo "Updating SPB"
+    cd /home/ec2-user/ShitPostBot/
+    sudo git pull
+    PIDS=$(echo | pgrep -f "spb_bot.py")
+    sudo kill -9 $PIDS
+    sudo nohup python3 spb_bot.py &
+elif [ $1 == "tpb" ]
+then
+    echo "Updating TPB"
+    cd /home/ec2-user/TickerBotProd/TickerPriceBot/
+    sudo git pull
+    PIDS=$(echo | pgrep -f "tpb_bot.py")
+    sudo kill -9 $PIDS
+    sudo nohup python3 tpb_bot.py &
+elif [ $1 == "cc" ]
+then
+    echo "Updating CC"
+    cd /home/ec2-user/CommandCenter/bot/
+    sudo git pull
+    PIDS=$(echo | pgrep -f "cc_bot.py")
+    sudo kill -9 $PIDS
+    sudo nohup python3 cc_bot.py &    
+fi
