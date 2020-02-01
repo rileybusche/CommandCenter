415a75429ce56a407e2c64dc820dc2498ddcfd9a
rileybusche <riley.busche@gmail.com>
2020-02-01 10:30:11 -0600

    Changing name of bot

---
+++ bot/cc_bot.py
@@ -0,0 +1,37 @@
+import discord
+import os
+import subprocess
+import json
+
+with open('/home/ec2-user/creds/creds.json') as file:
+    creds = json.load(file)
+
+token = creds['Credentials']['Command Center']['Token']
+
+client = discord.Client()
+
+@client.event
+async def on_message(message):
+
+    author = message.author
+    channel = message.channel
+    msg =  message.content.strip().lower()
+
+    if author == client.user:
+        return
+    
+    if msg == "!metrics":
+        # metrics = os.popen('cat iostat').read()
+        metrics = subprocess.check_output('iostat', shell=True).decode('utf-8')
+        await channel.send(f'```fix\n{metrics}```')
+
+@client.event
+async def on_ready():
+    global connected_guilds
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
+
+client.run(token)
