486c28927f4247578cf33c2a569ade7b83c4f820
rileybusche <riley.busche@gmail.com>
2020-02-01 15:44:46 -0600

    Updating metrics

--- bot/bot_controls.py
+++ bot/bot_controls.py
@@ -2,6 +2,9 @@ import os
 
 def start_bots():
     os.system('sh ./scripts/start_bots.sh')
+    bots = ['tbp', 'spb']
+    for bot in bots:
+        update_bot(bot)
 
 def update_bot(name):
     cmd = f'sh ./scripts/update.sh {name}'
