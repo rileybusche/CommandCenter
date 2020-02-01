52333d963b67c8cda5010004a4f2e1a6f2340205
rileybusche <riley.busche@gmail.com>
2020-01-31 20:37:35 -0600

    Adding iostat metric command

--- bot/bot.py
+++ bot/bot.py
@@ -2,4 +2,26 @@ import discord
 import os
 import json
 
-with open('/home/ec2-user/creds/creds.json')
+with open('/home/ec2-user/creds/creds.json') as file:
+    creds = json.load(file)
+
+token = creds['Credentials']['Command Center']['Token']
+
+client = discord.Client()
+
+@client.event
+async def on_message(message):
+
+    author = message.author
+    channel = message.channel
+    msg =  message.content.strip().lower()
+
+    if author == client.user:
+        return
+    
+    if msg == "!metrics":
+        metrics = os.system('iostat')
+        await channel.send(f'```fix\n{metrics}```')
+
+
+client.run(token)
