ce5ce37a56a5b6d879930e7c50d5f3414cf560ae
rileybusche <riley.busche@gmail.com>
2020-12-19 12:38:17 -0600

    Adding in new restart params

--- bot/cc_bot.py
+++ bot/cc_bot.py
@@ -53,9 +53,8 @@ async def on_message(message):
                     if instance.state['Name'] == 'running':
                         await channel.send(f'```fix\nServer is already running | IP : {instance.public_ip_address}```')
                     elif instance.state['Name'] == 'stopped':
-                        instance.start()
-                        while instance.public_ip_address == None:
-                            time.sleep(10)
+                        instance.start()                        
+                        time.sleep(10)
                         await channel.send(f'```fix\nIP : {instance.public_ip_address}```')
     # Stops the Minecraft EC2 server
     if msg == '!minecraft_stop' and author == bot_owner:
